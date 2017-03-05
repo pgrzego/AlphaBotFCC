69b9f77e08fe03098debdb880b81dc5521b21396
Piotr Grzegorzewski <pgrzegorzewski.wenow@gmail.com>
2017-03-05 14:23:51 +0100

    Bot starts and posts to SLAck_CHANNEL_NAME a meow! message

--- server/app.js
+++ server/app.js
@@ -1,12 +1,18 @@
 'use strict'
 
+require('dotenv').config()
+
 var SlackBot = require('slackbots');
 
 var bot = new SlackBot({
-  token: '',
-  name: ''
+  token: process.env.SLACK_TOKEN,
+  name: process.env.SLACK_NAME
 })
 
 bot.on('start', function() {
-  
+  var params = {
+    icon_emoji: ':lion_face:'
+  };
+
+  bot.postMessageToChannel(process.env.SLACK_CHANNEL_NAME, 'meow!', params);
 })
