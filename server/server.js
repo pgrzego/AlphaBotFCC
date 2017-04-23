0d24252d9df66e1b0d2e901db5fba51371178bc6
Piotr Grzegorzewski <pgrzegorzewski.wenow@gmail.com>
2017-04-23 12:36:56 +0200

    PORT needs to be defined in the .env file

--- server/server.js
+++ server/server.js
@@ -2,6 +2,6 @@
 
 const app = require('./app');
 
-const PORT = parseInt(process.env.PORT) || 3000;
+const PORT = parseInt(process.env.PORT);
 
 app().listen(PORT, console.log(`Process ${process.pid} listening on port ${PORT}`));
